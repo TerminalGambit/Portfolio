7933b72325e090935115c193ec32a556650a94e2
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-07 20:35:16 +0100

    Update App.js and asset-manifest.json

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import logo from './logo.svg';
-import GitHubContributions from './components/GitHubContributions';
 
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <p>
           Welcome to my portfolio! Explore my projects and learn more about me.
         </p>
-        <GitHubContributions />
         <a
           className="App-link"
           href="https://github.com/TerminalGambit"
