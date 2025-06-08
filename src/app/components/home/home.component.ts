72ef5aacdef7d4c39896e53b5810842db13abf46
TerminalGambit <jackmassey.monaco@outlook.com>
2025-06-08 18:49:53 +0200

    Update Angular project structure and dependencies; added Angular Material and CDK, configured assets in angular.json, and set up routing with a Home component. Enhanced styles with Material typography and updated index.html for font integration.

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule, MatGridListModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
